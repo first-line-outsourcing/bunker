65ad2ad069cf2bc0db4af163505f6c64c90c5c7d
Andrew Zaikin <zaikin.andry@gmail.com>
2020-10-24 13:43:42 +0300

    add amplify to vue

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue';
+// @ts-ignore
+import aws_exports from './aws-exports';
+
 Vue.config.productionTip = false;
+Amplify.configure(aws_exports);
 
 new Vue({
   router,
