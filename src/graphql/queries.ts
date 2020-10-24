16f4efc8ba1ac39b59a830daa7962e268ac069d1
Andrew Zaikin <zaikin.andry@gmail.com>
2020-10-24 17:17:05 +0300

    add models and set up qgl test env

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -2,28 +2,226 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const getTodo = /* GraphQL */ `
-  query GetTodo($id: ID!) {
-    getTodo(id: $id) {
+export const getUser = /* GraphQL */ `
+  query GetUser($id: ID!) {
+    getUser(id: $id) {
       id
       name
-      description
+      players {
+        items {
+          id
+          gameID
+          userID
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
       createdAt
       updatedAt
     }
   }
 `;
-export const listTodos = /* GraphQL */ `
-  query ListTodos(
-    $filter: ModelTodoFilterInput
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUserFilterInput
     $limit: Int
     $nextToken: String
   ) {
-    listTodos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
       items {
         id
         name
-        description
+        players {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getGame = /* GraphQL */ `
+  query GetGame($id: ID!) {
+    getGame(id: $id) {
+      id
+      host {
+        id
+        gameID
+        userID
+        user {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        cards {
+          nextToken
+        }
+        game {
+          id
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      players {
+        items {
+          id
+          gameID
+          userID
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listGames = /* GraphQL */ `
+  query ListGames(
+    $filter: ModelGameFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listGames(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        host {
+          id
+          gameID
+          userID
+          createdAt
+          updatedAt
+        }
+        players {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getPlayer = /* GraphQL */ `
+  query GetPlayer($id: ID!) {
+    getPlayer(id: $id) {
+      id
+      gameID
+      userID
+      user {
+        id
+        name
+        players {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      cards {
+        items {
+          id
+          cardID
+          playerID
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      game {
+        id
+        host {
+          id
+          gameID
+          userID
+          createdAt
+          updatedAt
+        }
+        players {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listPlayers = /* GraphQL */ `
+  query ListPlayers(
+    $filter: ModelPlayerFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listPlayers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        gameID
+        userID
+        user {
+          id
+          name
+          createdAt
+          updatedAt
+        }
+        cards {
+          nextToken
+        }
+        game {
+          id
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getCard = /* GraphQL */ `
+  query GetCard($id: ID!) {
+    getCard(id: $id) {
+      id
+      category
+      title
+      text
+      players {
+        items {
+          id
+          cardID
+          playerID
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listCards = /* GraphQL */ `
+  query ListCards(
+    $filter: ModelCardFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listCards(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        category
+        title
+        text
+        players {
+          nextToken
+        }
         createdAt
         updatedAt
       }
