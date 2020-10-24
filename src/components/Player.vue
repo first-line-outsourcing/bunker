89518b910577bf63ef27b0e311099c3d46c2795d
emahota-flo <emahota@flo.team>
2020-10-24 14:11:16 +0300

    created player component, removed hello world

---
+++ src/components/Player.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title">{{ player }}</h5>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+export interface Player {
+  id: number;
+  name: string;
+}
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private player!: string;
+}
+</script>
+
+<style scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div>
+    <Player player="Magic"></Player>
+    <span>{{ list }}</span>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Player from "@/components/Player.vue";
 
 @Component({
   components: {
-    HelloWorld
+    Player
   }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  list = 123;
+}
 </script>
